31cb147cc8e0884846ac591b750eb922c6c03b18
limheechan <decodeode@gmail.com>
2022-07-15 00:01:02 +0900

    Initial Commit

---
+++ nalCCi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nalCCi
+//
+//  Created by 임희찬 on 2022/07/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
