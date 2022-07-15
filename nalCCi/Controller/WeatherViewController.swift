c581e79a79f066d6cbefa9d220486a28e36daa58
limheechan <decodeode@gmail.com>
2022-07-15 16:30:46 +0900

    edit textFiled

--- nalCCi/Controller/WeatherViewController.swift
+++ nalCCi/Controller/WeatherViewController.swift
@@ -35,10 +35,11 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     }
     // textFiled 검색을 헀는지에 대한 유효성 검사
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        if serchTextFiled.text != ""{
+        
+        if textField.text != ""{
             return true
         }else{
-            serchTextFiled.placeholder = "도시를 검색 해보세요"
+            textField.placeholder = "도시를 검색 해보세요"
             return false
         }
     }
